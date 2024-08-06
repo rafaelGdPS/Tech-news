e7ad0b87dc90408b851f9663884750b9fd2c1524
Rafael <rafaelgermanio@gmail.com>
2024-08-06 13:15:41 -0300

    feat: implementando fetch

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,10 +1,23 @@
+import requests
+import time
+
 # Requisito 1
+
+
 def fetch(url):
-    """Seu código deve vir aqui"""
-    raise NotImplementedError
-
-
+    time.sleep(1)
+    try:
+        response = requests.get(url, timeout=3)
+        response.raise_for_status()
+        if response.status_code == 200:
+            return response.text
+    except requests.ReadTimeout:
+        return None
+    except requests.exceptions.HTTPError:
+        return None
 # Requisito 2
+
+
 def scrape_updates(html_content):
     """Seu código deve vir aqui"""
     raise NotImplementedError
